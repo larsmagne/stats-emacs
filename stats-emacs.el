1ede143f74895be0f8529288efcd5f27c0677da1
Lars Ingebrigtsen <larsi@gnus.org>
2016-05-03 18:41:31 +0200

    Add verbiage

--- stats-emacs.el
+++ stats-emacs.el
@@ -1,3 +1,30 @@
+;;; stats-emacs.el --- computing Emacs bug statistics
+;; Copyright (C) 2016 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: extensions, processes
+
+;; This file is not part of GNU Emacs.
+
+;; stats-emacs.el is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; stats-emacs.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
 
 (defun stats-emacs-get ()
   (apply 'debbugs-get-status (debbugs-get-bugs :archive "both")))
@@ -92,3 +119,6 @@
       (setq time (- time month))))
   (string-to-number (format-time-string "%Y%m%d" time)))
 
+(provide 'stats-emacs)
+
+;;; stats-emacs.el ends here
